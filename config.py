26285f4da6050d5f5dc1ab724d17c3da50eadd89
Alexander Teseyko <alexteko@gmail.com>
2017-06-24 14:14:14 +0300

    Config changed

--- config.py
+++ config.py
@@ -5,4 +5,9 @@ DEFAULT_IP = '10.160.18.{}/24'
 SSH_KEYS = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtbCa4zzsFlQgPmghFLR0FPEwD0XUKWdsQ9UZCVTLBNMtOxkd77aqeLT/f29ICGMnf' \
            'MDV6SfFaxN/uWJukQ0onPTNHTQwyJsxGZdrKBByhS0jBp4SXfoU6KH1gA/m8CykC0WSBzdJRT/1RemYAuC+AzklLthEO/F8gnC97QAWm' \
            'ix7govUrJWFQZ9k8pgybB3YUFu3SSqt/Q5PGfxFW8jmkpxFWi6DrBa4Yfuu7oP7d0p4+nYHbhTRPO0E1sXzZPBijXaVOzFJybgB/pYhX' \
-           '8JcpBrVPd3LffVIPfeFiC8b4dk3cX3ITkuzDQn423BzEOfrnlHj2R+WouvXKaHFM+bld root@delta\n'
+           '8JcpBrVPd3LffVIPfeFiC8b4dk3cX3ITkuzDQn423BzEOfrnlHj2R+WouvXKaHFM+bld root@delta\n' \
+           'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDT9XqjDnmzGjNNvaPuedpZ+diaU2xRrDZod+FLbqgv5qGy10uyChVbKxUofSDm5SlnY' \
+           'IZCbK+Y896oDTMogSaqja3sYY6CuIlu6qFLPdTTcbOJ6938hfSPKXBDQXdi7tAIyufEHR6fE+XwJUjHCfH4k5zgnjG5FFtONwhSid/kdU' \
+           'IBUKpZ8hQODHT/XbDawrlVps41PFIwlg8GDK0v/mHTwtrlTycuohXSPCJhWKkbIHh/ojjqick302HSReICdjsDGBQRiomf+2oU87JhK4z' \
+           'TR0vsSJ52V3+ztsTzTOncjlqNGklgQwZhRhmLn8KGB4U0KUFz0SzLhmS//2M2mgeX alex@alexs-iMac.home\n' \
+           ''
