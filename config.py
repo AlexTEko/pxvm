16d49f153493de3d0c96da2109d95e77da1545d5
Alexander Teseyko <alexteko@gmail.com>
2017-06-27 14:16:24 +0300

    Add default ssh key

--- config.py
+++ config.py
@@ -10,4 +10,8 @@ SSH_KEYS = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtbCa4zzsFlQgPmghFLR0FPEwD0XUK
            'IZCbK+Y896oDTMogSaqja3sYY6CuIlu6qFLPdTTcbOJ6938hfSPKXBDQXdi7tAIyufEHR6fE+XwJUjHCfH4k5zgnjG5FFtONwhSid/kdU' \
            'IBUKpZ8hQODHT/XbDawrlVps41PFIwlg8GDK0v/mHTwtrlTycuohXSPCJhWKkbIHh/ojjqick302HSReICdjsDGBQRiomf+2oU87JhK4z' \
            'TR0vsSJ52V3+ztsTzTOncjlqNGklgQwZhRhmLn8KGB4U0KUFz0SzLhmS//2M2mgeX alex@alexs-iMac.home\n' \
+           'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnjocWNhB9c1F3kbKZRn6y8U+Ms3AE8tPTtkvZ5ToNGaVXNSJj/L76aaLbm4GqW+yNB' \
+           'tviop9Uz57Ip5IoCRv3g5U8PCCYlcQq9/nMdyvXUcIku70AEBPa46Fzzo0OtZLjonZp9npjZpz7OXdwl4aF48Md8kQns8oNhIBNNl+/LH' \
+           'otJ8dS+O9W8jN2uwWerrjoUv81ariUEC9h/lrM2n8G9oV++W9JYigMqn8ndl/XNICrYFZIaOITXH/fxGW5gKC8TkzsZ0MdEC4mdLu0/zB' \
+           'OKjo4INxBT2mNkhAs6wbAkS81bJcCA5MFjbtrgw9SC3IQSbZ2QcySw4OfxkVG7Kxh alex@Alexanders-Air.skynet\n' \
            ''
