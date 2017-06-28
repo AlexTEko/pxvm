f1d2cb809c225ead9bb48dc3c489d1634c2b4baa
Alexander Teseyko <alexteko@gmail.com>
2017-06-28 15:41:47 +0300

    Fix autotest

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,10 +1,9 @@
-
 import config
-
+import tests.test_credentials
 from pxvm.libs.proxlib import Prox
 
 
 def test_version():
-    px = Prox(config.PX_HOST, user='autotest@pve', password='autotest')
+    px = Prox(config.PX_HOST, user=tests.test_credentials.autotest_user, password=tests.test_credentials.autotest_pw)
     response = px.get_version()
     assert 'version' in response
